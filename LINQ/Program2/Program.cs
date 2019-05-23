dc5929615ae4e3a4b656cb313ee2d6abcdc8f89c
Poornima-Capgemini <Poornima8878@gmail.com>
2019-05-23 02:05:17 -0700

    Files added

---
+++ LINQ/Program2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[]
+            {
+                "Sangeetha","Jithendra","Sai","Siva","Arun","Pallavi","Dhivya","Aruna"
+            };
+            var result1 = from e in names select e;
+            Console.WriteLine("Names are :");
+            foreach (var v in result1)
+            {
+                Console.WriteLine(v);
+            }
+            var result2 = from e in names where e.StartsWith("S") select e;
+            Console.WriteLine("\r\nFiltered :");
+            foreach (var v in result2)
+            {
+                Console.WriteLine(v);
+            }
+            var result3 = from e in names orderby e select e;
+            Console.WriteLine("\r\nSorted :");
+            foreach (var v in result3)
+            {
+                Console.WriteLine(v);
+            }
+
+            var result4 = from e in names orderby e descending select e;
+            Console.WriteLine("\r\nSorted :");
+            foreach (var v in result4)
+            {
+                Console.WriteLine(v);
+            }
+
+        }
+    }
+}
---
+++ LINQ/Program3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employ> lstEmploy = new List<Employ>()
+           {
+               new Employ{Empno=1,Name="Jithu",Dept="Dotnet",Desig="Devloper",Basic=88234},
+               new Employ{Empno=2,Name="Ram",Dept="SQL",Desig="Programmer",Basic=63244},
+               new Employ{Empno=3,Name="Sada",Dept="Dotnet",Desig="Devloper",Basic=36778},
+               new Employ{Empno=4,Name="Anitha",Dept="SQL",Desig="Admin",Basic=44365},
+               new Employ{Empno=5,Name="Arun",Dept="Dotnet",Desig="Devloper",Basic=78333}
+           };
+            var result = from e in lstEmploy select e;
+            Console.WriteLine("All Employees ");
+            foreach(var v in result)
+            {
+                Console.WriteLine(v.Empno+" "+v.Name+" "+v.Dept+" "+v.Desig+" "+v.Basic);
+            }
+
+            var result1 = from e in lstEmploy select new { e.Empno, e.Name, e.Basic };
+            Console.WriteLine("\r\nProjected Fieds ");
+            foreach (var v in result1)
+            {
+                Console.WriteLine(v.Empno + " " + v.Name + " "+ v.Basic);
+            }
+
+            var result2 = from e in lstEmploy where e.Basic >= 5000 select e;
+            Console.WriteLine("\r\nFiltered Records ");
+            foreach (var v in result2)
+            {
+                Console.WriteLine(v.Empno + " " + v.Name + " " + v.Dept + " " + v.Desig + " " + v.Basic);
+            }
+
+            var result4 = from e in lstEmploy orderby e.Name select e;
+            Console.WriteLine("\r\nSorted Employees ");
+            foreach (var v in result4)
+            {
+                Console.WriteLine(v.Empno + " " + v.Name + " " + v.Dept + " " + v.Desig + " " + v.Basic);
+            }
+        }
+    }
+}
